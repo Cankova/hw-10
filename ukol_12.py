ee47606de33e026006565fcebc229fee5f9a0a8a
Lenka Cankova <xcanko00@stud.feec.vutbr.cz>
2021-05-02 19:28:50 +0200

    1. commit

--- ukol_12.py
+++ ukol_12.py
@@ -1,11 +1,35 @@
+import os
+
+def rad_data(file_name, custom_idx):
+    """
+
+    :param file_name:
+    :param custom_idx:
+    :return:
+    """
+
+    return
+
+
+def tokenize(unsorted_list):
+    """
+
+    :param unsorted_list:
+    :return:
+    """
+
+    return sorted_sequence, frequency
+
 
 
 def main():
     pass
 
     # read data
+    row = read_data()
 
     # sort sequence
+    sort = tokenize(row)
 
 
 if __name__ == "__main__":
